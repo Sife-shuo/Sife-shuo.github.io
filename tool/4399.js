51b58de559db27f9aaef5ff0830152ca2187eaee
Sife-shuo <88375951+Sife-shuo@users.noreply.github.com>
2023-07-27 16:45:15 +0800

    Update 4399.js

--- tool/4399.js
+++ tool/4399.js
@@ -981,7 +981,6 @@ $(function(){
 		(function () {
 			window._windon_handler = setInterval( function() {
 				var before = new Date();
-				debugger;
 				var after = new Date();
 				if (after.getTime() - before.getTime() > 100) {
 					if (after.getTime() - before.getTime() > 2000) {
